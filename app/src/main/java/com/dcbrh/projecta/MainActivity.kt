6af9cc8bef22a3c7b6247c25ea42e950d2eb78e3
delacruzbrandon <dcbrh.ph@gmail.com>
2022-01-25 23:07:38 +0800

    Edited ProductModels

--- app/src/main/java/com/dcbrh/projecta/MainActivity.kt
+++ app/src/main/java/com/dcbrh/projecta/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.dcbrh.projecta
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
@@ -11,6 +14,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dcbrh.projecta.model.ProductModel
@@ -44,16 +49,24 @@ private fun mainLayout(_product: ProductModel) {
 @Composable
 fun Product(_product: ProductModel) {
     Row {
-        Text(text = "${_product.productName}!")
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "${_product.productInfo}!")
+        Image(
+            painter = painterResource(id = _product.productImage),
+            contentDescription = "Product Image"
+        )
+        Column() {
+            Text(text = "${_product.productBrand}")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "${_product.productName}")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "${_product.productPrice}")
+        }
     }
-    Text(text = "${_product.productPrice}")
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ProjectATheme {
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    ProjectATheme {
+//    }
+//}
--- app/src/main/java/com/dcbrh/projecta/model/ProductModel.kt
+++ app/src/main/java/com/dcbrh/projecta/model/ProductModel.kt
@@ -3,7 +3,7 @@ package com.dcbrh.projecta.model
 data class ProductModel(
     var productId: Int,
     var productImage: Int,
+    var productBrand: String,
     var productName: String,
-    var productInfo: String,
     var productPrice: String
 )
