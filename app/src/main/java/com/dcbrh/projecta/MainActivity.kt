8be6eac4cfb2dc231cd86cf59bd60d633b42c399
delacruzbrandon <dcbrh.ph@gmail.com>
2022-01-27 15:15:57 +0800

    Added ProductList

--- app/src/main/java/com/dcbrh/projecta/MainActivity.kt
+++ app/src/main/java/com/dcbrh/projecta/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.dcbrh.projecta
 
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,49 +22,71 @@ import com.dcbrh.projecta.ui.theme.ProjectATheme
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var testProduct: ProductModel
+    private lateinit var testProduct: ProductModel
+    private lateinit var testProductList: List<ProductModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         testProduct = ProductTestData.productList[0]
+        testProductList = ProductTestData.productList
 
-        setContent {mainLayout(testProduct)}
+        setContent { MainLayout(testProductList) }
     }
 
 
 }
 
 @Composable
-private fun mainLayout(_product: ProductModel) {
+private fun MainLayout(_productList: List<ProductModel>) {
     ProjectATheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
+            ProductList(_productList = _productList)
+        }
+    }
+}
+
+@Composable
+fun ProductList(_productList: List<ProductModel>) {
+    LazyColumn {
+        items(_productList) { _product ->
             Product(_product = _product)
         }
     }
+
 }
 
 @Composable
 fun Product(_product: ProductModel) {
-    Row {
+
+    var isExpanded by remember { mutableStateOf(true) }
+
+    Row(modifier = Modifier.padding(16.dp)) {
         Image(
             painter = painterResource(id = _product.productImage),
-            contentDescription = "Product Image"
+            contentDescription = "Product Image",
+            modifier = Modifier
+                .size(80.dp)
         )
-        Column() {
-            Text(text = "${_product.productBrand}")
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "${_product.productName}")
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "${_product.productPrice}")
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = _product.productBrand,
+                modifier = if(!isExpanded) Modifier.height(0.dp) else Modifier.fillMaxHeight()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = _product.productName)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = _product.productPrice)
         }
     }
 
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    ProjectATheme {
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProjectATheme {
+        ProductList(_productList = ProductTestData.productList)
+    }
+}
--- app/src/main/java/com/dcbrh/projecta/repo/ProductTestData.kt
+++ app/src/main/java/com/dcbrh/projecta/repo/ProductTestData.kt
@@ -9,16 +9,12 @@ object ProductTestData {
     private val productModel = ProductModel(
         1,
         R.mipmap.testbag,
-        R.string.testProductBrand.toString(),
-        R.string.testProductName.toString(),
-        R.string.testProductPrice.toString()
+        "Louis Vuitton",
+        "Pégase Légère 55 Business",
+        "$3,574"
     )
 
     val productList: List<ProductModel> = listOf(
-        productModel,
-        productModel,
-        productModel,
-        productModel,
         productModel,
         productModel,
         productModel,
